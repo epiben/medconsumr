9fd99a29e8dcfe8f005705e5686d399d4b5d9427
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2022-03-10 10:46:08 +0100

    Remove erroneous argument

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ for (p in pkgs) {
 }
 
 for (p in pkgs) {
-  library(p, character = TRUE, repos = "http://cran.us.r-project.org")
+  library(p, character = TRUE)
 }
 
 dummy_data <- "		Total	2007	2008	2009	2010	2011	2012	2013	2014	2015	2016	2017	2018	2019
