9f283241b253d88581d767c43a2acbd20fcd7598
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2022-03-13 21:15:03 +0100

    Improve documentation

--- R/launch_app.R
+++ R/launch_app.R
@@ -3,6 +3,14 @@
 #' Later, perhaps put the logic into separate function and invoke them. For now,
 #' all logic is just here.
 #'
+#' @param object not used, perhaps later used for passing results to the app.
+#' @param rstudio logical, if `TRUE` the app uses the RStudio default, otherwise
+#'   the browser is launched.
+#' @param ... passed on to shiny::runApp
+#'
+#' Set host = "0.0.0.0" and port at your choosing to expose the app to others on
+#' your LAN.
+#'
 #' @export
 
 launch_app <- function(object, rstudio = TRUE, ...) {
